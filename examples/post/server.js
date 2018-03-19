ad9f4edc326fd1612b9bf9790a0324f3dbc8e9da
yinm <yinm.cminor9@gmail.com>
2018-03-19 22:19:38 +0900

    Check post

--- examples/post/server.js
+++ examples/post/server.js
@@ -1,16 +1,16 @@
-module.exports = function (req, res) {
-  var data = '';
-  
-  req.on('data', function (chunk) {
-    data += chunk;
-  });
+module.exports = (req, res) => {
+  let data = '';
 
-  req.on('end', function () {
-    console.log('POST data received');
+  req.on('data', (chunk) => {
+    data += chunk
+  })
+
+  req.on('end', () => {
+    console.log('POST  data received')
     res.writeHead(200, {
       'Content-Type': 'text/json'
-    });
-    res.write(JSON.stringify(data));
-    res.end();
-  });
-};
+    })
+    res.write(JSON.stringify(data))
+    res.end()
+  })
+}
